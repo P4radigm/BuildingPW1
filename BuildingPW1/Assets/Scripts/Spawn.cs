bce8d38199eb67664e4a6781f31421b43b0ca1f4
P4radigm <leon.vanoldenborgh@student.hku.nl>
2019-03-03 22:25:14 +0100

    Added spawner

---
+++ BuildingPW1/Assets/Scripts/Spawn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public Transform[] spawnLocations;
+    public GameObject[] whatToSpawnPrefab;
+    public GameObject[] whatToSpawnClone;
+
+    private void Start()
+    {
+        spawnSmth(3, 13);
+    }
+
+    void spawnSmth(int currentKey, int kindOfKey)
+    {
+        whatToSpawnClone[currentKey] = Instantiate(whatToSpawnPrefab[currentKey], spawnLocations[kindOfKey].transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
+        whatToSpawnClone[currentKey].name = currentKey.ToString() + '-' + kindOfKey.ToString();
+    }
+
+
+}
